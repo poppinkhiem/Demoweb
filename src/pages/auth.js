17306806804c8fe833ec1c702912c3d08688022d
Khiem Nguyen <81703148+HashmatNguyen@users.noreply.github.com>
2022-04-14 18:28:48 +0700

    login ok

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,6 +1,21 @@
-import React from "react";
+
 import { useNavigate } from "react-router-dom";
-import Login from "./login";
+
+
+
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(8).max(12).required(),
+}); 
+
+
+
 const Auth = ({ authenticate }) => {
   const navigate = useNavigate(); 
 
@@ -8,16 +23,36 @@ const Auth = ({ authenticate }) => {
     authenticate();
     navigate("home");
   };
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
+  function handleSignup(e) {
+    
+    const pass = localStorage.setItem("password", 'khiem');
+    const user = localStorage.setItem("username", 8888888);
+  }
+
+  const onSubmit = (data) => console.log(data);
+
+
 
   return (
-    <div style={{float: 'right'}}>
-      <h2> Click here to Login </h2>
-      <button onClick={onClick}>Login</button>
-      
-    </div>
+    <>
+    <form onSubmit={handleSubmit(onClick)} style={{float: 'right'}}>
+      <input {...register('email')} type="email" />
+      <p>{errors.email?.message}</p>
+      <input {...register('password', { required: true })} />
+      {errors.email && <p>email name is required.</p>}
+      <input {...register('age', { pattern: /\d+/ })} />
+      <p>{errors.password?.message}</p>
+      <input type="submit" />
+      <button onClick={handleSignup}> Sign Up</button>
+         
+
+
+    </form>
+
+    </>
   );
-};
-
-
-
+}
 export default Auth;
--- src/pages/login.js
+++ src/pages/login.js
@@ -11,14 +11,20 @@ const schema = yup.object().shape({
 
 
 
-export default function Login() {
+export default function Login({logout}) {
   const { register, handleSubmit, formState: { errors }, reset } = useForm({
     resolver: yupResolver(schema),
   });
-  
+  function handleSignup(e) {
+    
+    const pass = localStorage.setItem("password", 'khiem');
+    const user = localStorage.setItem("username", 8888888);
+  }
+
   const onSubmit = (data) => console.log(data);
 
   return (
+    <>
     <form onSubmit={handleSubmit(onSubmit)} style={{float: 'right'}}>
       <input {...register('email')} type="email" />
       <p>{errors.email?.message}</p>
@@ -27,6 +33,12 @@ export default function Login() {
       <input {...register('age', { pattern: /\d+/ })} />
       <p>{errors.password?.message}</p>
       <input type="submit" />
+      <button onClick={handleSignup}> Sign Up</button>
+         
+
+
     </form>
+
+    </>
   );
 }
